022d86e458ad3cf5c1d77c9a1ffb40bcc19564de
RomanPryimuk <roman.pryimuk@resmed.com>
2022-12-14 14:45:56 +0200

    iterator pattern

--- src/iterator/Company.java
+++ src/iterator/Company.java
@@ -10,40 +10,6 @@ public class Company implements Iterable {
 
     @Override
     public Iterator getIterator() {
-        return new EmployeeIterator();
-    }
-
-
-    public static class Employee {
-        String name;
-        String lastName;
-
-        public Employee(String name, String lastName) {
-            this.name = name;
-            this.lastName = lastName;
-        }
-
-        @Override
-        public String toString() {
-            return "Employee{" +
-                    "name='" + name + '\'' +
-                    ", lastName='" + lastName + '\'' +
-                    '}';
-        }
-    }
-
-
-    private class EmployeeIterator implements Iterator {
-        int index;
-
-        @Override
-        public boolean hasNext() {
-            return employee.length > index;
-        }
-
-        @Override
-        public Object next() {
-            return employee[index++];
-        }
+        return new EmployeeIterator(employee);
     }
 }
--- src/iterator/IteratorCompanyRunner.java
+++ src/iterator/IteratorCompanyRunner.java
@@ -3,13 +3,13 @@ package iterator;
 public class IteratorCompanyRunner {
     public static void main(String[] args) {
 
-        Company.Employee[] employees = new Company.Employee[]{
-                new Company.Employee("Roman", "Pryimuk"),
-                new Company.Employee("Den", "Lion"),
-                new Company.Employee("Chin", "Pion"),
-                new Company.Employee("Pin", "Cava"),
-                new Company.Employee("Din", "Nint"),
-                new Company.Employee("Fin", "Pint")
+        Employee[] employees = new Employee[]{
+                new Employee("Roman", "Pryimuk"),
+                new Employee("Den", "Lion"),
+                new Employee("Chin", "Pion"),
+                new Employee("Pin", "Cava"),
+                new Employee("Din", "Nint"),
+                new Employee("Fin", "Pint")
         };
 
         Company company = new Company(employees);
